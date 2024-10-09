15870b3406ad4edaf7b40e85bd685ca782658679
Elliott Katane <elliott.katane@gmail.com>
2024-10-09 14:38:22 -0400

    setup de la bdd

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import {  Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ ConfigModule.forRoot(), // Pour charger les variables d'environnement
+    MongooseModule.forRoot(process.env.MONGODB_URI),],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  onModuleInit() {
+    console.log('Connected to MongoDB successfully!');
+  }
+}
