dd75e78b7100359fe2113ef3de85a309cf26888a
GuillaumeGandolfi <gandolfi.guillaume@gmail.com>
2024-10-24 16:18:29 -0400

    feat(auth): :sparkles: implement AuthGuard and custom decorator for user authentication

--- backend/src/event/event.controller.ts
+++ backend/src/event/event.controller.ts
@@ -1,14 +1,29 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { EventService } from './event.service';
 import { CreateEventDto } from './create-event.dto';
-
+import { AuthGuard } from '../common/guards/auth.guard';
+import { GetUserId } from '../common/decorators/get-user.decorator';
+import { Types } from 'mongoose';
 @Controller('events')
+// On applique le guard pour sécuriser les routes
+@UseGuards(AuthGuard)
 export class EventController {
   constructor(private readonly eventService: EventService) {}
 
   @Post()
-  async create(@Body() createEventDto: CreateEventDto) {
-    return this.eventService.createEvent(createEventDto);
+  async create(
+    @Body() createEventDto: CreateEventDto,
+    @GetUserId() userId: Types.ObjectId,
+  ) {
+    return this.eventService.createEvent(createEventDto, userId);
   }
 
   @Get()
