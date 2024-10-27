200b19a1bcab45d20e72db0eb8fe8a9fe380f344
GuillaumeGandolfi <gandolfi.guillaume@gmail.com>
2024-10-27 11:43:20 -0400

    feat(auth): add login DTO and implement password hashing for user creation

--- backend/src/user/schemas/user.schema.ts
+++ backend/src/user/schemas/user.schema.ts
@@ -1,8 +1,11 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 
 @Schema()
 export class User extends Document {
+  @Prop({ type: Types.ObjectId, auto: true })
+  _id: Types.ObjectId;
+
   @Prop({ required: true })
   name: string;
 
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -2,14 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
 import { User } from './schemas/user.schema';
-import { CreateUserDto } from './create-user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
   async createUser(createUserDto: CreateUserDto): Promise<User> {
-    const createdUser = new this.userModel(createUserDto);
+    // Hacher le mot de passe avant de créer l'utilisateur
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+
+    // Créer l'utilisateur avec le mot de passe haché
+    const createdUser = new this.userModel({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+
     return createdUser.save();
   }
 
